c717c2325a589794c6851c24eec41c8da1464e22
Farrel <farrel.alfaridziii@gmail.com>
2026-07-22 01:31:25 +0700

    feat: add form handling and input validation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,7 +8,31 @@ def about():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        nama="Farrel",
+        umur=18,
+        jurusan="Teknik Informatika",
+        saldo=0,
+        hobi=[
+            "Ngoding",
+            "Main Game",
+            "Belajar Linux"
+        ]
+    )
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    if request.method == "POST":
+        jumlah = request.form["jumlah"]
+        kategori = request.form["kategori"]
+        catatan = request.form["catatan"]
+        if jumlah == "":
+            return "jumlah tidak boleh kosong"
+        print(jumlah)
+        print(kategori)
+        print(catatan)
+    return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
